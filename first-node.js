e4d2ec847ad84e2c93314f9b9079cf7bb5352d90
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-08-11 12:27:10 +0530

    created response content for 3 diff url

--- first-node.js
+++ first-node.js
@@ -1,1 +1,21 @@
-console.log("hello world from node js ")
+const http = require('http')
+
+const server = http.createServer((req,res) =>{
+    console.log(req);
+    res.setHeader('Content-Type','text/html');
+    if(req.url==='/')
+    {
+        res.write('<html><head><title>NodeJS</title><body><h1>Response from node server</h1></body></html>')
+    }
+    if(req.url==='/home')
+    {
+        res.write('<html><head><title>NodeJS</title><body><h1>Welcome Home</h1></body></html>')
+    }
+    if(req.url==='/about')
+    {
+        res.write('<html><head><title>NodeJS</title><body><h1>About Page</h1></body></html>')
+    }
+    res.end()
+})
+
+server.listen(3000)
